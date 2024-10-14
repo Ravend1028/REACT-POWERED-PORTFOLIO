5e01caa75e97b86641b71b3c1e904a7709f5b17b
Ravend1028 <ravendavid.rd30@gmail.com>
2024-10-14 20:13:33 +0800

    animations ready and optimization (lazy loading, preloader)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <main>
       <div className='container mx-auto p-4 flex flex-col justify-center items-center space-y-10 2xl:p-6'>
         <div className='flex flex-row justify-center items-center '>
-          <div className=' border-4 border-black w-2/3 sm:w-1/2 md:w-2/5 xl:w-1/3'>
+          <div className=' border-4 border-black w-3/4 sm:w-1/2 md:w-2/5 xl:w-1/3'>
             <img className="rounded-xl transform rotate-3 border-2 border-black" src={ Raven } alt="raven-image" />
           </div>
         </div>
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -7,17 +7,32 @@ const Socials = () => {
     <section>
       <div className='container mx-auto p-4 flex flex-col justify-center items-center space-y-16 lg:space-y-8 2xl:space-y-24 md:p-6'>
         <div className='flex flex-row justify-center items-center space-x-7'>
-          <button className='bg-black text-white p-2 hover:text-amber-500 rounded-lg md:rounded-full'>
-            <i class="fa-brands fa-github text-2xl md:text-3xl 2xl:text-4xl"></i>
-          </button>
+          <a 
+            href="https://www.linkedin.com/in/raven-david-511407279/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className='bg-black text-white p-2 hover:text-amber-500 rounded-lg md:rounded-full'
+          >
+            <i className="fa-brands fa-linkedin text-2xl md:text-3xl 2xl:text-4xl"></i>
+          </a>
 
-          <button className='bg-black text-white p-2 hover:text-amber-500 rounded-lg md:rounded-full'>
-            <i class="fa-brands fa-linkedin text-2xl md:text-3xl 2xl:text-4xl"></i>
-          </button>
+          <a 
+            href="https://github.com/Ravend1028" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className='bg-black text-white p-2 hover:text-amber-500 rounded-lg md:rounded-full'
+          >
+            <i className="fa-brands fa-github text-2xl md:text-3xl 2xl:text-4xl"></i>
+          </a>
 
-          <button className='bg-black text-white p-2 hover:text-amber-500 rounded-lg md:rounded-full'>
-            <i class="fa-brands fa-facebook text-2xl md:text-3xl 2xl:text-4xl"></i>
-          </button>
+          <a 
+            href="https://www.facebook.com/raven.david.28" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className='bg-black text-white p-2 hover:text-amber-500 rounded-lg md:rounded-full'
+          >
+            <i className="fa-brands fa-facebook text-2xl md:text-3xl 2xl:text-4xl"></i>
+          </a>
         </div>
 
         <div>
